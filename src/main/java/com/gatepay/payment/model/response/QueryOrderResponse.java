62493f0af59de16bc1f2abe3a2275c10d8704d60
gatepay2025 <zj-be@gate.me>
2025-07-14 16:34:02 +0800

    Query order response.

--- src/main/java/com/gatepay/payment/model/response/QueryOrderResponse.java
+++ src/main/java/com/gatepay/payment/model/response/QueryOrderResponse.java
@@ -1,4 +1,223 @@
 package com.gatepay.payment.model.response;
 
 public class QueryOrderResponse {
+
+    private String prepayId;
+    private int merchantId;
+    private String merchantTradeNo;
+    private String transactionId;
+    private String goodsName;
+    private String currency;
+    private String orderAmount;
+    private String status;
+    private int createTime;
+    private int expireTime;
+    private int transactTime;
+    private String orderName;
+    private String payCurrency;
+    private String payAmount;
+    private String expectCurrency;
+    private String actualCurrency;
+    private String actualAmount;
+    private String rate;
+    private String payChan;
+    private String payAccount;
+    private String appName;
+    private String appLogo;
+    private String inUsdt;
+    private String channelId;
+
+
+    public String getPrepayId() {
+        return prepayId;
+    }
+
+    public void setPrepayId(String prepayId) {
+        this.prepayId = prepayId;
+    }
+
+    public int getMerchantId() {
+        return merchantId;
+    }
+
+    public void setMerchantId(int merchantId) {
+        this.merchantId = merchantId;
+    }
+
+    public String getMerchantTradeNo() {
+        return merchantTradeNo;
+    }
+
+    public void setMerchantTradeNo(String merchantTradeNo) {
+        this.merchantTradeNo = merchantTradeNo;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getOrderAmount() {
+        return orderAmount;
+    }
+
+    public void setOrderAmount(String orderAmount) {
+        this.orderAmount = orderAmount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(int createTime) {
+        this.createTime = createTime;
+    }
+
+    public int getExpireTime() {
+        return expireTime;
+    }
+
+    public void setExpireTime(int expireTime) {
+        this.expireTime = expireTime;
+    }
+
+    public int getTransactTime() {
+        return transactTime;
+    }
+
+    public void setTransactTime(int transactTime) {
+        this.transactTime = transactTime;
+    }
+
+    public String getOrderName() {
+        return orderName;
+    }
+
+    public void setOrderName(String orderName) {
+        this.orderName = orderName;
+    }
+
+    public String getPayCurrency() {
+        return payCurrency;
+    }
+
+    public void setPayCurrency(String payCurrency) {
+        this.payCurrency = payCurrency;
+    }
+
+    public String getPayAmount() {
+        return payAmount;
+    }
+
+    public void setPayAmount(String payAmount) {
+        this.payAmount = payAmount;
+    }
+
+    public String getExpectCurrency() {
+        return expectCurrency;
+    }
+
+    public void setExpectCurrency(String expectCurrency) {
+        this.expectCurrency = expectCurrency;
+    }
+
+    public String getActualCurrency() {
+        return actualCurrency;
+    }
+
+    public void setActualCurrency(String actualCurrency) {
+        this.actualCurrency = actualCurrency;
+    }
+
+    public String getActualAmount() {
+        return actualAmount;
+    }
+
+    public void setActualAmount(String actualAmount) {
+        this.actualAmount = actualAmount;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+
+    public String getPayChan() {
+        return payChan;
+    }
+
+    public void setPayChan(String payChan) {
+        this.payChan = payChan;
+    }
+
+    public String getPayAccount() {
+        return payAccount;
+    }
+
+    public void setPayAccount(String payAccount) {
+        this.payAccount = payAccount;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getAppLogo() {
+        return appLogo;
+    }
+
+    public void setAppLogo(String appLogo) {
+        this.appLogo = appLogo;
+    }
+
+    public String getInUsdt() {
+        return inUsdt;
+    }
+
+    public void setInUsdt(String inUsdt) {
+        this.inUsdt = inUsdt;
+    }
+
+    public String getChannelId() {
+        return channelId;
+    }
+
+    public void setChannelId(String channelId) {
+        this.channelId = channelId;
+    }
+
 }
