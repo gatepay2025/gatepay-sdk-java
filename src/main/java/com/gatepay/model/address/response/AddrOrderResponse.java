134155f944a7269c75cc24a8db9a5d4f1a574f5f
gatepay2025 <zj-be@gate.me>
2025-07-14 17:41:29 +0800

    AddrOrderResponse.

---
+++ src/main/java/com/gatepay/model/address/response/AddrOrderResponse.java
@@ -0,0 +1,45 @@
+package com.gatepay.model.address.response;
+
+import com.gatepay.model.address.Chain;
+
+public class AddrOrderResponse {
+
+    private String prepayID;
+    private String terminalType;
+    private int expireTime;
+    private Chain chainInfo;
+
+
+    public String getPrepayID() {
+        return prepayID;
+    }
+
+    public void setPrepayID(String prepayID) {
+        this.prepayID = prepayID;
+    }
+
+    public String getTerminalType() {
+        return terminalType;
+    }
+
+    public void setTerminalType(String terminalType) {
+        this.terminalType = terminalType;
+    }
+
+    public int getExpireTime() {
+        return expireTime;
+    }
+
+    public void setExpireTime(int expireTime) {
+        this.expireTime = expireTime;
+    }
+
+    public Chain getChainInfo() {
+        return chainInfo;
+    }
+
+    public void setChainInfo(Chain chainInfo) {
+        this.chainInfo = chainInfo;
+    }
+
+}
