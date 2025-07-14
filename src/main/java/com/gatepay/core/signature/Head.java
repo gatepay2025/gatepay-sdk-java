51a2b77354becdb3d74e3d99d27bf2984611cdf8
gatepay2025 <zj-be@gate.me>
2025-07-14 15:41:01 +0800

    http header.

---
+++ src/main/java/com/gatepay/core/signature/Head.java
@@ -0,0 +1,17 @@
+package com.gatepay.core.signature;
+
+public class Head {
+
+    public static final String HEADER_GATEPAY_API_KEY = "X-GatePay-Certificate-SN";
+
+    public static final String HEADER_GATEPAY_CLIENT_ID = "X-GatePay-Certificate-ClientId";
+
+    public static final String HEADER_GATEPAY_TIMESTAMP = "X-GatePay-Timestamp";
+
+    public static final String HEADER_GATEPAY_NONCE = "X-GatePay-Nonce";
+
+    public static final String HEADER_GATEPAY_SIGNATURE = "X-GatePay-Signature";
+
+    public static final String HEADER_GATE_MERCHANT_ID = "X-GatePay-ChannelId";
+
+}
