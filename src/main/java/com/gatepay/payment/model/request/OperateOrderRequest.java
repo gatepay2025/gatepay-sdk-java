6578d26c814cbbb6edf5b0c806b6907986386a0c
gatepay2025 <zj-be@gate.me>
2025-07-14 15:51:06 +0800

    OperateOrderRequest.

--- src/main/java/com/gatepay/payment/model/request/OperateOrderRequest.java
+++ src/main/java/com/gatepay/payment/model/request/OperateOrderRequest.java
@@ -1,4 +1,27 @@
 package com.gatepay.payment.model.request;
 
-public class OperateOrderRequest {
+import com.gatepay.common.BaseRequest;
+
+
+public class OperateOrderRequest extends BaseRequest {
+
+    private String prepayId;
+    private String merchantTradeNo;
+
+    public String getPrepayId() {
+        return this.prepayId;
+    }
+
+    public void setPrepayId(String prepayId) {
+        this.prepayId = prepayId;
+    }
+
+    public String getMerchantTradeNo() {
+        return this.merchantTradeNo;
+    }
+
+    public void setMerchantTradeNo(String merchantTradeNo) {
+        this.merchantTradeNo = merchantTradeNo;
+    }
+
 }
