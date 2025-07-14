ad79ddf0cbf754079163dc7952ead3c8e8b8277d
gatepay2025 <zj-be@gate.me>
2025-07-14 17:20:28 +0800

    EnvRequest and GoodsRequest.

---
+++ src/main/java/com/gatepay/model/address/request/EnvRequest.java
@@ -0,0 +1,25 @@
+package com.gatepay.model.address.request;
+
+public class EnvRequest {
+
+    private String terminalType;
+    private String scene;
+
+
+    public String getTerminalType() {
+        return terminalType;
+    }
+
+    public void setTerminalType(String terminalType) {
+        this.terminalType = terminalType;
+    }
+
+    public String getScene() {
+        return scene;
+    }
+
+    public void setScene(String scene) {
+        this.scene = scene;
+    }
+
+}
---
+++ src/main/java/com/gatepay/model/address/request/GoodsRequest.java
@@ -0,0 +1,23 @@
+package com.gatepay.model.address.request;
+
+public class GoodsRequest {
+
+    private String goodsName;
+    private String goodsDetail;
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+
+    public String getGoodsDetail() {
+        return goodsDetail;
+    }
+
+    public void setGoodsDetail(String goodsDetail) {
+        this.goodsDetail = goodsDetail;
+    }
+}
