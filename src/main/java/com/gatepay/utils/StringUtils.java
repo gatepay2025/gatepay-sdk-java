ca1f910345657c7f28d8283ac648ee8cae911085
gatepay2025 <zj-be@gate.me>
2025-07-14 15:43:15 +0800

    String utils.

---
+++ src/main/java/com/gatepay/utils/StringUtils.java
@@ -0,0 +1,24 @@
+package com.gatepay.utils;
+
+import java.util.List;
+
+public class StringUtils {
+
+    public static String joinStrSepByDoubleQuot(List<String> strList) {
+        if (strList == null || strList.isEmpty()) {
+            return "\"\"";
+        }
+        StringBuilder sb = new StringBuilder("\"");
+        sb.append(String.join("\", \"", strList));
+        sb.append("\"");
+        return sb.toString();
+    }
+
+    public static boolean isStringInList(String str, List<String> strs) {
+        if (strs == null) {
+            return Boolean.FALSE;
+        }
+        return strs.contains(str);
+    }
+
+}
