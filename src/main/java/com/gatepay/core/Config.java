320086a5cf23dcacf3d654cd3028cbb620fd59ad
gatepay2025 <zj-be@gate.me>
2025-07-15 13:46:30 +0800

    Config and head.

---
+++ src/main/java/com/gatepay/core/Config.java
@@ -0,0 +1,43 @@
+package com.gatepay.core;
+
+import java.time.Duration;
+
+public class Config {
+
+    private String scheme;
+    private String endpoint;
+    private Duration timeout;
+
+
+    public Config() {
+        this.scheme = "https";
+        this.endpoint = Constants.DEFAULT_END_POINT;
+        this.timeout = Duration.ofSeconds(30);
+    }
+
+
+    public String getScheme() {
+        return scheme;
+    }
+
+    public void setScheme(String scheme) {
+        this.scheme = scheme;
+    }
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    public Duration getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(Duration timeout) {
+        this.timeout = timeout;
+    }
+
+}
