f64612051cd21e98503ccea90f45714848722184
gatepay2025 <zj-be@gate.me>
2025-07-15 11:50:25 +0800

    TransactionDetail.

---
+++ src/main/java/com/gatepay/model/address/TransactionDetail.java
@@ -0,0 +1,25 @@
+package com.gatepay.model.address;
+
+public class TransactionDetail {
+
+    private TxDetail inTerm;
+    private TxDetail outOfTerm;
+
+
+    public TxDetail getInTerm() {
+        return inTerm;
+    }
+
+    public void setInTerm(TxDetail inTerm) {
+        this.inTerm = inTerm;
+    }
+
+    public TxDetail getOutOfTerm() {
+        return outOfTerm;
+    }
+
+    public void setOutOfTerm(TxDetail outOfTerm) {
+        this.outOfTerm = outOfTerm;
+    }
+
+}
