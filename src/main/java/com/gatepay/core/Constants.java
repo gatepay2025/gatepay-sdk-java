ecef46139ac2039732bb8eee1eb943ac1de0a2cd
gatepay2025 <zj-be@gate.me>
2025-07-15 13:34:10 +0800

    Constants.

---
+++ src/main/java/com/gatepay/core/Constants.java
@@ -0,0 +1,26 @@
+package com.gatepay.core;
+
+public class Constants {
+
+    public static final String DEFAULT_END_POINT = "http://dev.halftrust.xyz/gfpay";
+
+    public static final String SCHEME_HTTP = "http";
+    public static final String SCHEME_HTTPS = "https";
+    public static final String METHOD_GET = "GET";
+    public static final String METHOD_PUT = "PUT";
+    public static final String METHOD_POST = "POST";
+    public static final String METHOD_DELETE = "DELETE";
+    public static final String METHOD_PATCH = "PATCH";
+    public static final String METHOD_HEAD = "HEAD";
+
+
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String ACCEPT = "Accept";                 // Header 中的 Accept 字段
+    public static final String CONTENT_TYPE = "Content-Type";     // Header 中的 ContentType 字段
+    public static final String USER_AGENT = "User-Agent";
+
+
+    public static final String VERSION = "0.0.1";                 // SDK 版本号
+    public static final String USER_AGENT_FORMAT = "GatePay-SDK-Java/%s (%s) Java/%s"; // UserAgent中的信息
+
+}
