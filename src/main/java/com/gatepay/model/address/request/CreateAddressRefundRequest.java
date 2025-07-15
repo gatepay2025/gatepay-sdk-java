99089a24432cc7f2f4beb1be21f0ab716faa2635
gatepay2025 <zj-be@gate.me>
2025-07-15 10:42:01 +0800

    CreateAddressRefundRequest.

---
+++ src/main/java/com/gatepay/model/address/request/CreateAddressRefundRequest.java
@@ -0,0 +1,56 @@
+package com.gatepay.model.address.request;
+
+import com.gatepay.common.BaseRequest;
+
+import java.math.BigDecimal;
+
+public class CreateAddressRefundRequest extends BaseRequest {
+
+    private String refundRequestId;
+    private String prepayId;
+    private BigDecimal refundAmount;
+    private String refundReason;
+    private int receiverId;
+
+
+    public String getRefundRequestId() {
+        return refundRequestId;
+    }
+
+    public void setRefundRequestId(String refundRequestId) {
+        this.refundRequestId = refundRequestId;
+    }
+
+    public String getPrepayId() {
+        return prepayId;
+    }
+
+    public void setPrepayId(String prepayId) {
+        this.prepayId = prepayId;
+    }
+
+    public BigDecimal getRefundAmount() {
+        return refundAmount;
+    }
+
+    public void setRefundAmount(BigDecimal refundAmount) {
+        this.refundAmount = refundAmount;
+    }
+
+    public String getRefundReason() {
+        return refundReason;
+    }
+
+    public void setRefundReason(String refundReason) {
+        this.refundReason = refundReason;
+    }
+
+    public int getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(int receiverId) {
+        this.receiverId = receiverId;
+    }
+
+}
