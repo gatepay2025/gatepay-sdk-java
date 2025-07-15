980d7d586f8c1bab0e4ade90380a99dec9d33138
gatepay2025 <zj-be@gate.me>
2025-07-15 09:44:24 +0800

    ConfirmItem & ChainTransactionInfo.

---
+++ src/main/java/com/gatepay/model/address/ChainTransactionInfo.java
@@ -0,0 +1,27 @@
+package com.gatepay.model.address;
+
+import java.math.BigDecimal;
+
+public class ChainTransactionInfo {
+
+    private BigDecimal doneAmount;
+    private ConfirmItem[] confirmingList;
+
+
+    public BigDecimal getDoneAmount() {
+        return doneAmount;
+    }
+
+    public void setDoneAmount(BigDecimal doneAmount) {
+        this.doneAmount = doneAmount;
+    }
+
+    public ConfirmItem[] getConfirmingList() {
+        return confirmingList;
+    }
+
+    public void setConfirmingList(ConfirmItem[] confirmingList) {
+        this.confirmingList = confirmingList;
+    }
+
+}
