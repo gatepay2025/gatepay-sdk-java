2d8acaebaccf24071b8e1fb000642ad56877d5f3
gatepay2025 <zj-be@gate.me>
2025-07-16 13:30:07 +0800

    Api address.

---
+++ src/main/java/com/gatepay/service/address/ApiAddress.java
@@ -0,0 +1,280 @@
+package com.gatepay.service.address;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gatepay.common.GatePayConstants;
+import com.gatepay.core.signature.Signer;
+import com.gatepay.service.address.model.request.ChainsReq;
+import com.gatepay.service.address.model.request.CreateOrderReq;
+import com.gatepay.service.address.model.request.QueryOrderReq;
+import com.gatepay.service.address.model.request.SupportedConvertCurrenciesReq;
+import com.gatepay.service.address.model.response.*;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.Map;
+
+
+/**
+ *
+ */
+public class ApiAddress {
+
+    private final String baseUrl;
+    private final String apiKey;
+    private final String apiSecret;
+    private final HttpClient httpClient;
+    private final ObjectMapper objectMapper;
+
+
+    public ApiAddress(String baseUrl, String apiKey, String apiSecret) {
+        this.baseUrl = baseUrl;
+        this.apiKey = apiKey;
+        this.apiSecret = apiSecret;
+        this.httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .connectTimeout(Duration.ofSeconds(30))
+                .build();
+        this.objectMapper = new ObjectMapper();
+    }
+
+
+
+
+    /**
+     * 查询支持链列表
+     *
+     * @param request
+     * @return
+     */
+    public ChainsResp getAddressChains(ChainsReq request) {
+        // 构建请求URL
+        String url = GatePayConstants.DEFAULT_END_POINT + GatePayConstants.END_POINT_ADDRESS_CHAINS + "?currency=" + request.getCurrency();
+        String queryString = ""; // buildQueryStrByGet(null);
+        String secretKey = "Mz6M_q4AkDnZCSoTDo03A6OtWzN5ut8_Uix3jyVjxAU=";
+        //String url = baseUrl + endpoint + (queryString.isEmpty() ? "" : "?" + queryString);
+
+        // 构建请求头
+        long timestamp = System.currentTimeMillis();
+        String signature = Signer.verifySignature(String.valueOf(System.currentTimeMillis()), "1234567890", queryString, secretKey);
+
+        // 发送请求
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header(GatePayConstants.HEADER_CONTENT_TYPE, "application/json")
+                .header(GatePayConstants.HEADER_GATEPAY_TIMESTAMP, String.valueOf(timestamp))
+                .header(GatePayConstants.HEADER_GATEPAY_NONCE, "1234567890")
+                .header(GatePayConstants.HEADER_GATEPAY_SIGNATURE, signature)
+                .header(GatePayConstants.HEADER_GATEPAY_CERTIFICATE_CLIENT_ID, "mZ96D37oKk-HrWJc")   // apiKey)
+                .GET()
+                .build();
+        try {
+            HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 解析响应
+        return new ChainsResp();
+    }
+
+
+    /**
+     *
+     * @return
+     */
+    public CurrenciesResp getAddressCurrencies() {
+        // 构建请求URL
+        String url = GatePayConstants.DEFAULT_END_POINT + GatePayConstants.END_POINT_ADDRESS_CURRENCIES;
+
+        String queryString = "";  // buildQueryString(null);
+        String secretKey = "Mz6M_q4AkDnZCSoTDo03A6OtWzN5ut8_Uix3jyVjxAU=";
+        //String url = baseUrl + endpoint + (queryString.isEmpty() ? "" : "?" + queryString);
+
+        // 构建请求头
+        long timestamp = System.currentTimeMillis();
+        String signature = Signer.verifySignature(String.valueOf(System.currentTimeMillis()), "1234567890", queryString, secretKey);
+
+        // 发送请求
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header(GatePayConstants.HEADER_CONTENT_TYPE, "application/json")
+                .header(GatePayConstants.HEADER_GATEPAY_TIMESTAMP, String.valueOf(timestamp))
+                .header(GatePayConstants.HEADER_GATEPAY_NONCE, "1234567890")
+                .header(GatePayConstants.HEADER_GATEPAY_SIGNATURE, signature)
+                .header(GatePayConstants.HEADER_GATEPAY_CERTIFICATE_CLIENT_ID, "mZ96D37oKk-HrWJc")   // apiKey)
+                .GET()
+                .build();
+        try {
+            HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 解析响应
+        return new CurrenciesResp();
+    }
+
+
+
+    /**
+     * 创建闪兑地址支付单之前，根据订单币种查询支持闪兑的币种，用户从支持闪兑的币种列表中选择实际支付币种创建闪兑支付订单
+     *
+     * @param request
+     * currency 订单币种
+     *
+     * @return
+     * currencies 支持闪兑到订单币种的币种列表
+     */
+    public SupportedConvertCurrenciesResp getSupportedConvertCurrencies(SupportedConvertCurrenciesReq request) {
+        // 构建请求URL
+        String url = GatePayConstants.DEFAULT_END_POINT + GatePayConstants.END_POINT_ADDRESS_SUPPORTED_CONVERT_CURRENCIES + "?currency=" + request.getCurrency();
+
+        String queryString = "";  // buildQueryString(null);
+        String secretKey = "Mz6M_q4AkDnZCSoTDo03A6OtWzN5ut8_Uix3jyVjxAU=";
+        //String url = baseUrl + endpoint + (queryString.isEmpty() ? "" : "?" + queryString);
+
+        // 构建请求头
+        long timestamp = System.currentTimeMillis();
+        String signature = Signer.verifySignature(String.valueOf(timestamp), "1234567890", queryString, secretKey);
+
+        // 发送请求
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header(GatePayConstants.HEADER_CONTENT_TYPE, "application/json")
+                .header(GatePayConstants.HEADER_GATEPAY_TIMESTAMP, String.valueOf(timestamp))
+                .header(GatePayConstants.HEADER_GATEPAY_NONCE, "1234567890")
+                .header(GatePayConstants.HEADER_GATEPAY_SIGNATURE, signature)
+                .header(GatePayConstants.HEADER_GATEPAY_CERTIFICATE_CLIENT_ID, "mZ96D37oKk-HrWJc")   // apiKey)
+                .GET()
+                .build();
+        try {
+            HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 解析响应
+        return new SupportedConvertCurrenciesResp();
+    }
+
+
+    /**
+     *
+     * 创建地址支付订单/下单
+     *
+     */
+    public CreateOrderResp createOrder(CreateOrderReq request) throws JsonProcessingException {
+        // 构建请求URL
+        String url = GatePayConstants.DEFAULT_END_POINT + GatePayConstants.END_POINT_ADDRESS_CREATE_ORDER;
+
+        String queryString = objectMapper.writeValueAsString(request);  // buildQueryString(null);
+        String secretKey = "Mz6M_q4AkDnZCSoTDo03A6OtWzN5ut8_Uix3jyVjxAU=";
+        //String url = baseUrl + endpoint + (queryString.isEmpty() ? "" : "?" + queryString);
+
+        // 构建请求头
+        long timestamp = 1752742852L;// System.currentTimeMillis();
+        String signature = Signer.verifySignature(String.valueOf(timestamp), "1234567890", queryString, secretKey);
+
+        // 发送请求
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header(GatePayConstants.HEADER_CONTENT_TYPE, "application/json")
+                .header(GatePayConstants.HEADER_GATEPAY_TIMESTAMP, String.valueOf(timestamp))
+                .header(GatePayConstants.HEADER_GATEPAY_NONCE, "1234567890")
+                .header(GatePayConstants.HEADER_GATEPAY_SIGNATURE, signature)
+                .header(GatePayConstants.HEADER_GATEPAY_CERTIFICATE_CLIENT_ID, "mZ96D37oKk-HrWJc")   // apiKey)
+                .POST(HttpRequest.BodyPublishers.ofString(objectMapper.writeValueAsString(request)))
+                .build();
+        try {
+            HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 解析响应
+        return new CreateOrderResp();
+    }
+
+
+    /**
+     * 查询地址支付订单
+     */
+    public QueryOrderResp queryOrder(QueryOrderReq request) {
+        // 构建请求URL
+        String url = GatePayConstants.DEFAULT_END_POINT + GatePayConstants.END_POINT_ADDRESS_QUERY_ORDER + "?prepayId=" + request.getPrepayId() + "&merchantTradeNo=" + request.getMerchantTradeNo();
+
+        String queryString = "";  // buildQueryString(null);
+        String secretKey = "Mz6M_q4AkDnZCSoTDo03A6OtWzN5ut8_Uix3jyVjxAU=";
+        //String url = baseUrl + endpoint + (queryString.isEmpty() ? "" : "?" + queryString);
+
+        // 构建请求头
+        long timestamp = System.currentTimeMillis();
+        String signature = Signer.verifySignature(String.valueOf(System.currentTimeMillis()), "1234567890", queryString, secretKey);
+
+        // 发送请求
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header(GatePayConstants.HEADER_CONTENT_TYPE, "application/json")
+                .header(GatePayConstants.HEADER_GATEPAY_TIMESTAMP, String.valueOf(timestamp))
+                .header(GatePayConstants.HEADER_GATEPAY_NONCE, "1234567890")
+                .header(GatePayConstants.HEADER_GATEPAY_SIGNATURE, signature)
+                .header(GatePayConstants.HEADER_GATEPAY_CERTIFICATE_CLIENT_ID, "mZ96D37oKk-HrWJc")   // apiKey)
+                .GET()
+                .build();
+        try {
+            HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 解析响应
+        return new QueryOrderResp();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * 构建查询字符串
+     *
+     * @param params 参数映射
+     * @return 查询字符串
+     */
+    private String buildQueryStrByGet(Map<String, String> params) {
+        if (params.isEmpty()) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            if (sb.length() > 0) {
+                sb.append("&");
+            }
+            sb.append(entry.getKey()).append("=").append(entry.getValue());
+        }
+        return sb.toString();
+    }
+
+}
