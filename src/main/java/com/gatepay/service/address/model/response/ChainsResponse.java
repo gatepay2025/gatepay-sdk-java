d2431980e4974aab8eb34b9f108671164386e6dc
gatepay2025 <zj-be@gate.me>
2025-07-16 10:30:47 +0800

    Arrange packages and class names.

--- src/main/java/com/gatepay/service/address/model/response/ChainsResponse.java
+++ src/main/java/com/gatepay/service/address/model/response/ChainsResponse.java
@@ -6,6 +6,25 @@ public class ChainsResponse {
     private ChainNameItem[] chains;
 
 
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public ChainNameItem[] getChains() {
+        return chains;
+    }
+
+    public void setChains(ChainNameItem[] chains) {
+        this.chains = chains;
+    }
+
+
+
+
     private class ChainNameItem {
         private String chain;
         private String currency;
@@ -13,6 +32,55 @@ public class ChainsResponse {
         private String symbol;
         private String explorerUrl;
         private String showChainNameEn;
+
+
+        public String getChain() {
+            return chain;
+        }
+
+        public void setChain(String chain) {
+            this.chain = chain;
+        }
+
+        public String getCurrency() {
+            return currency;
+        }
+
+        public void setCurrency(String currency) {
+            this.currency = currency;
+        }
+
+        public String getFullCurrType() {
+            return fullCurrType;
+        }
+
+        public void setFullCurrType(String fullCurrType) {
+            this.fullCurrType = fullCurrType;
+        }
+
+        public String getSymbol() {
+            return symbol;
+        }
+
+        public void setSymbol(String symbol) {
+            this.symbol = symbol;
+        }
+
+        public String getExplorerUrl() {
+            return explorerUrl;
+        }
+
+        public void setExplorerUrl(String explorerUrl) {
+            this.explorerUrl = explorerUrl;
+        }
+
+        public String getShowChainNameEn() {
+            return showChainNameEn;
+        }
+
+        public void setShowChainNameEn(String showChainNameEn) {
+            this.showChainNameEn = showChainNameEn;
+        }
     }
 
 }
