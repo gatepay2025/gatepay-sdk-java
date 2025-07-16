c97fed5c66d782954ee882a1ea7eaf7aae7cd790
gatepay2025 <zj-be@gate.me>
2025-07-16 22:39:26 +0800

    Query order response.

---
+++ src/main/java/com/gatepay/service/web/model/response/QueryOrderResp.java
@@ -0,0 +1,20 @@
+package com.gatepay.service.web.model.response;
+
+// todo
+public class QueryOrderResp {
+
+    // SUCCESS 或者 FAIL
+    private String status;
+
+    // 出错代码
+    private String code;
+
+
+
+
+    // data query order return type	否	支付订单信息
+
+    // 错误信息
+    private String errorMessage;
+
+}
