8888e05af65d4c54d6aaf6d38848b15086f5c38b
gatepay2025 <zj-be@gate.me>
2025-07-17 09:35:43 +0800

    Web query order request.

--- src/main/java/com/gatepay/service/web/model/request/QueryOrderReq.java
+++ src/main/java/com/gatepay/service/web/model/request/QueryOrderReq.java
@@ -2,15 +2,22 @@ package com.gatepay.service.web.model.request;
 
 import com.gatepay.common.BaseRequest;
 import com.gatepay.common.GatePayConstants;
+import com.gatepay.common.annotation.GatePayParam;
 
 public class QueryOrderReq extends BaseRequest {
 
+    @GatePayParam
     private String prepayId;
+
+    @GatePayParam
     private String merchantTradeNo;
 
+
     public QueryOrderReq() {
-        this.requestUrl = super.requestUrl + GatePayConstants.END_POINT_PAY_ORDER_QUERY;
+        this.requestUrl = GatePayConstants.END_POINT_PAY_ORDER_QUERY;
+        this.requestMethod = GatePayConstants.METHOD_GET;
     }
+
 
     public String getPrepayId() {
         return prepayId;
