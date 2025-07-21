192ad9f85a5584af33f32ddda7a3f5929a421559
gatepay2025 <zj-be@gate.me>
2025-07-21 17:00:57 +0800

    convert currency.

--- src/main/java/com/gatepay/api/convert/model/response/QueryCurrencyResp.java
+++ src/main/java/com/gatepay/api/convert/model/response/QueryCurrencyResp.java
@@ -2,5 +2,16 @@ package com.gatepay.api.convert.model.response;
 
 import com.gatepay.common.BaseResponse;
 
-public class QueryCurrencyResp extends BaseResponse {
+public class QueryCurrencyResp extends BaseResponse<QueryCurrencyResp> {
+
+    private String[] currency;
+
+    public String[] getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String[] currency) {
+        this.currency = currency;
+    }
+
 }
