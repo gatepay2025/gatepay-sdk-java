bb4c3a6a0b3058991963a249374be26e6069bfde
gatepay2025 <zj-be@gate.me>
2025-07-21 16:34:35 +0800

    bill update and delete response.

--- src/main/java/com/gatepay/api/channelmanage/model/response/DeleteResp.java
+++ src/main/java/com/gatepay/api/channelmanage/model/response/DeleteResp.java
@@ -3,5 +3,5 @@ package com.gatepay.api.channelmanage.model.response;
 import com.gatepay.common.BaseResponse;
 
 
-public class DeleteResp extends BaseResponse {
+public class DeleteResp extends BaseResponse<DeleteResp> {
 }
--- src/main/java/com/gatepay/api/channelmanage/model/response/UpdateResp.java
+++ src/main/java/com/gatepay/api/channelmanage/model/response/UpdateResp.java
@@ -2,5 +2,5 @@ package com.gatepay.api.channelmanage.model.response;
 
 import com.gatepay.common.BaseResponse;
 
-public class UpdateResp extends BaseResponse {
+public class UpdateResp extends BaseResponse<UpdateResp> {
 }
