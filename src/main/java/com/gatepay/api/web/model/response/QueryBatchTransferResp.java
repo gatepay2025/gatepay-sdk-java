fc8f1b8d8544ddebb1c1a1e9ffa38aa151467f6d
gatepay2025 <zj-be@gate.me>
2025-07-22 13:45:23 +0800

    api web test.

--- src/main/java/com/gatepay/api/web/model/response/QueryBatchTransferResp.java
+++ src/main/java/com/gatepay/api/web/model/response/QueryBatchTransferResp.java
@@ -1,6 +1,76 @@
 package com.gatepay.api.web.model.response;
 
 import com.gatepay.common.BaseResponse;
+import java.util.Map;
 
-public class QueryBatchTransferResp extends BaseResponse {
+
+public class QueryBatchTransferResp extends BaseResponse<QueryBatchTransferResp> {
+
+    private String batch_id;
+    private long merchant_id;
+    private String merchant_batch_no;
+    private String status;
+    private String currency;
+    private String channel_id;
+    private Map[] orders_list;
+
+
+    public String getBatch_id() {
+        return batch_id;
+    }
+
+    public void setBatch_id(String batch_id) {
+        this.batch_id = batch_id;
+    }
+
+    public long getMerchant_id() {
+        return merchant_id;
+    }
+
+    public void setMerchant_id(long merchant_id) {
+        this.merchant_id = merchant_id;
+    }
+
+    public String getMerchant_batch_no() {
+        return merchant_batch_no;
+    }
+
+    public void setMerchant_batch_no(String merchant_batch_no) {
+        this.merchant_batch_no = merchant_batch_no;
+    }
+
+    @Override
+    public String getStatus() {
+        return status;
+    }
+
+    @Override
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getChannel_id() {
+        return channel_id;
+    }
+
+    public void setChannel_id(String channel_id) {
+        this.channel_id = channel_id;
+    }
+
+    public Map[] getOrders_list() {
+        return orders_list;
+    }
+
+    public void setOrders_list(Map[] orders_list) {
+        this.orders_list = orders_list;
+    }
+
 }
--- src/test/java/com/gatepay/api/web/ApiWebTest.java
+++ src/test/java/com/gatepay/api/web/ApiWebTest.java
@@ -2,6 +2,8 @@ package com.gatepay.api.web;
 
 import com.gatepay.api.web.model.BatchOrder;
 import com.gatepay.api.web.model.request.*;
+import com.gatepay.common.model.EnvReq;
+import com.gatepay.common.model.GoodsReq;
 import org.junit.jupiter.api.Test;
 
 
@@ -10,24 +12,42 @@ public class ApiWebTest {
     ApiWeb apiWeb = new ApiWeb("mZ96D37oKk-HrWJc");
 
 
+    // self test passed
     @Test
     public void testCreateOrder() {
+        EnvReq envReq = new EnvReq();
+        envReq.setTerminalType("MINIAPP");
+        GoodsReq goodsReq = new GoodsReq();
+        goodsReq.setGoodsName("25000000元宝");
+        goodsReq.setGoodsDetail("充值");
         CreateOrderReq createOrderReq = new CreateOrderReq();
+        createOrderReq.setMerchantTradeNo("1688217212j916");
+        createOrderReq.setCurrency("USDT");
+        createOrderReq.setOrderAmount("0.1");
+        createOrderReq.setEnv(envReq);
+        createOrderReq.setGoods(goodsReq);
+        createOrderReq.setExtendInfo("1_elbt01_16882172126048");
+        createOrderReq.setChannelId("123");
         apiWeb.createOrder(createOrderReq);
     }
 
+    // self test passed
     @Test
     public void testQueryOrder() {
         QueryOrderReq queryOrderReq = new QueryOrderReq();
+        queryOrderReq.setMerchantTradeNo("1688217212j916");
         apiWeb.queryOrder(queryOrderReq);
     }
 
+    // todo: test, GATEPAY_INVALID_ORDER_STATUS_EXPIRED
     @Test
     public void testCloseOrder() {
         CloseOrderReq closeOrderReq = new CloseOrderReq();
+        closeOrderReq.setMerchantTradeNo("1688217212j916");
         apiWeb.closeOrder(closeOrderReq);
     }
 
+    // todo: test ORDER_NOT_FOUND
     @Test
     public void testCreateRefund() {
         CreateRefundReq createRefundReq = new CreateRefundReq();
@@ -37,21 +57,23 @@ public class ApiWebTest {
         apiWeb.createRefund(createRefundReq);
     }
 
+    // todo: test, UNKNOWN_ERROR
     @Test
     public void testQueryRefund() {
         QueryRefundReq queryRefundReq = new QueryRefundReq();
-        queryRefundReq.setRefundRequestId("483902480932840785");
+        queryRefundReq.setRefundRequestId("156123911");
         apiWeb.queryRefund(queryRefundReq);
     }
 
+    // todo: where is the api doc ?
     @Test
     public void testCreateBatchTransfer() {
         BatchOrder batchOrder = new BatchOrder();
         batchOrder.setUser_id(6790011);
         batchOrder.setAmount("2");
         CreateBatchTransferReq createBatchTransferReq = new CreateBatchTransferReq();
-        createBatchTransferReq.setBatchid("13243243242444");
-        createBatchTransferReq.setMerchant_batch_no("192392929193500");
+        createBatchTransferReq.setBatchid("13243243242445");
+        createBatchTransferReq.setMerchant_batch_no("192392929193391");
         createBatchTransferReq.setBizscene("REWARDS");
         createBatchTransferReq.setMerchant_id("10002");
         createBatchTransferReq.setClient_id("");
@@ -62,6 +84,7 @@ public class ApiWebTest {
         apiWeb.createBatchTransfer(createBatchTransferReq);
     }
 
+    // self test passed
     @Test
     public void testQueryBatchTransfer() {
         QueryBatchTransferReq queryBatchTransferReq = new QueryBatchTransferReq();
@@ -71,6 +94,7 @@ public class ApiWebTest {
         apiWeb.queryBatchTransfer(queryBatchTransferReq);
     }
 
+    // self test passed
     @Test
     public void testQueryBalance() {
         QueryBalanceReq queryBalanceReq = new QueryBalanceReq();
