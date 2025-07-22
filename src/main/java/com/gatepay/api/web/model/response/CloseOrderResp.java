0251490654ed4f34073f6918d4c00b7f4a961d54
gatepay2025 <zj-be@gate.me>
2025-07-22 11:15:00 +0800

    web close order response.

--- src/main/java/com/gatepay/api/web/model/response/CloseOrderResp.java
+++ src/main/java/com/gatepay/api/web/model/response/CloseOrderResp.java
@@ -2,5 +2,16 @@ package com.gatepay.api.web.model.response;
 
 import com.gatepay.common.BaseResponse;
 
-public class CloseOrderResp extends BaseResponse {
+public class CloseOrderResp extends BaseResponse<CloseOrderResp> {
+
+    private String result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
 }
