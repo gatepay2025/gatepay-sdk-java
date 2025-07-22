edc42befcdf838923106b5a42299d225bca44bb0
gatepay2025 <zj-be@gate.me>
2025-07-22 11:14:28 +0800

    web close order request.

--- src/main/java/com/gatepay/api/web/model/request/CloseOrderReq.java
+++ src/main/java/com/gatepay/api/web/model/request/CloseOrderReq.java
@@ -1,17 +1,24 @@
 package com.gatepay.api.web.model.request;
 
 import com.gatepay.common.BaseRequest;
-import com.gatepay.common.annotation.GatePayParam;
 import com.gatepay.common.enums.GatePayApi;
 
 
 public class CloseOrderReq extends BaseRequest {
 
-    @GatePayParam
+    private String merchantTradeNo;
     private String prepayId;
 
     public CloseOrderReq() {
         this.api = GatePayApi.PAY_CLOSE_ORDER;
+    }
+
+    public String getMerchantTradeNo() {
+        return merchantTradeNo;
+    }
+
+    public void setMerchantTradeNo(String merchantTradeNo) {
+        this.merchantTradeNo = merchantTradeNo;
     }
 
     public String getPrepayId() {
