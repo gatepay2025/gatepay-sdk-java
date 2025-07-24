ee8c48eb01534bb0421446a9ed9955f2cd4f0454
gatepay2025 <zj-be@gate.me>
2025-07-24 13:37:23 +0800

    http builder

--- src/main/java/com/gatepay/core/GatePayHttpClient.java
+++ src/main/java/com/gatepay/core/GatePayHttpClient.java
@@ -33,38 +33,46 @@ public class GatePayHttpClient {
                 .header(GatePayConstants.HEADER_GATEPAY_TIMESTAMP, String.valueOf(timestamp))
                 .header(GatePayConstants.HEADER_GATEPAY_NONCE, nonce)
                 .header(GatePayConstants.HEADER_GATEPAY_CERTIFICATE_CLIENT_ID, this.gatePayConfig.getClientId());
-        if (GatePayConstants.METHOD_GET.equals(request.getApi().getHttpMethod()) || GatePayConstants.METHOD_DELETE.equals(request.getApi().getHttpMethod())) {
-            String paramStr = "";
-            Field[] declaredFields = request.getClass().getDeclaredFields();
-            if (declaredFields != null && declaredFields.length > 0)  {
-                for (Field field : declaredFields) {
-                    field.setAccessible(Boolean.TRUE);
-                    if (field.isAnnotationPresent(GatePayParam.class) && field.get(request) != null) {
-                        paramStr = paramStr == "" ? paramStr + "?" : paramStr + "&";
-                        paramStr = paramStr + field.getName() + "=" + field.get(request);
-                    }
-                }
-            }
-            builder.header(GatePayConstants.HEADER_GATEPAY_SIGNATURE, Signature.verifySignature(String.valueOf(timestamp), nonce, "", this.gatePayConfig.getCredential().getSecretKey()));
-            builder.uri(URI.create(this.gatePayConfig.getEndPoint() + request.getApi().getUrl() + paramStr));
-            return GatePayConstants.METHOD_GET.equals(request.getApi().getHttpMethod()) ? builder.GET().build() : builder.DELETE().build();
+        if (GatePayConstants.METHOD_GET.equals(request.getApi().getHttpMethod())) {
+            populateBuilderForGetDelete(request, builder, timestamp, nonce);
+            return builder.GET().build();
+        }
+        if (GatePayConstants.METHOD_DELETE.equals(request.getApi().getHttpMethod())) {
+            populateBuilderForGetDelete(request, builder, timestamp, nonce);
+            return builder.DELETE().build();
         }
         if (GatePayConstants.METHOD_POST.equals(request.getApi().getHttpMethod())) {
-            builder.header(GatePayConstants.HEADER_GATEPAY_API_KEY, this.gatePayConfig.getCredential().getApiKey());
-            builder.header(GatePayConstants.HEADER_GATEPAY_SIGNATURE, Signature.verifySignature(String.valueOf(timestamp), nonce, new ObjectMapper().writeValueAsString(request), this.gatePayConfig.getCredential().getSecretKey()));
-            builder.uri(URI.create(this.gatePayConfig.getEndPoint() + request.getApi().getUrl()));
+            populateBuilderForPostPut(request, builder, timestamp, nonce);
             return builder.POST(HttpRequest.BodyPublishers.ofString(new ObjectMapper().writeValueAsString(request))).build();
         }
         if (GatePayConstants.METHOD_PUT.equals(request.getApi().getHttpMethod())) {
-            builder.header(GatePayConstants.HEADER_GATEPAY_API_KEY, this.gatePayConfig.getCredential().getApiKey());
-            builder.header(GatePayConstants.HEADER_GATEPAY_SIGNATURE, Signature.verifySignature(String.valueOf(timestamp), nonce, new ObjectMapper().writeValueAsString(request), this.gatePayConfig.getCredential().getSecretKey()));
-            builder.uri(URI.create(this.gatePayConfig.getEndPoint() + request.getApi().getUrl()));
+            populateBuilderForPostPut(request, builder, timestamp, nonce);
             return builder.PUT(HttpRequest.BodyPublishers.ofString(new ObjectMapper().writeValueAsString(request))).build();
         }
         return null;
     }
 
+    private <T extends BaseRequest> void populateBuilderForGetDelete(T request, HttpRequest.Builder builder, long timestamp, String nonce) throws IllegalAccessException {
+        String paramStr = "";
+        Field[] declaredFields = request.getClass().getDeclaredFields();
+        if (declaredFields != null && declaredFields.length > 0)  {
+            for (Field field : declaredFields) {
+                field.setAccessible(Boolean.TRUE);
+                if (field.isAnnotationPresent(GatePayParam.class) && field.get(request) != null) {
+                    paramStr = paramStr == "" ? paramStr + "?" : paramStr + "&";
+                    paramStr = paramStr + field.getName() + "=" + field.get(request);
+                }
+            }
+        }
+        builder.header(GatePayConstants.HEADER_GATEPAY_SIGNATURE, Signature.verifySignature(String.valueOf(timestamp), nonce, "", this.gatePayConfig.getCredential().getSecretKey()));
+        builder.uri(URI.create(this.gatePayConfig.getEndPoint() + request.getApi().getUrl() + paramStr));
+    }
 
+    private <T extends BaseRequest> void populateBuilderForPostPut(T request, HttpRequest.Builder builder, long timestamp, String nonce) throws JsonProcessingException {
+        builder.header(GatePayConstants.HEADER_GATEPAY_API_KEY, this.gatePayConfig.getCredential().getApiKey());
+        builder.header(GatePayConstants.HEADER_GATEPAY_SIGNATURE, Signature.verifySignature(String.valueOf(timestamp), nonce, new ObjectMapper().writeValueAsString(request), this.gatePayConfig.getCredential().getSecretKey()));
+        builder.uri(URI.create(this.gatePayConfig.getEndPoint() + request.getApi().getUrl()));
+    }
 
     public HttpClient getHttpClient() {
         return httpClient;
