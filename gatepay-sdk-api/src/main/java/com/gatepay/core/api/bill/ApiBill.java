c3eb59ab0c10db975f12b4c788d46df7c1942ce3
gatepay2025 <zj-be@gate.me>
2025-07-26 23:06:21 +0800

    comments on api bill

--- gatepay-sdk-api/src/main/java/com/gatepay/core/api/bill/ApiBill.java
+++ gatepay-sdk-api/src/main/java/com/gatepay/core/api/bill/ApiBill.java
@@ -9,13 +9,22 @@ import com.gatepay.core.api.BaseApi;
 import com.gatepay.core.api.bill.model.req.QueryOrdersReq;
 import com.gatepay.core.api.bill.model.resp.QueryOrdersResp;
 
-
+/**
+ * @Description 账单api
+ * @Author ZJ-BE
+ * @Date 2025/7/25
+ */
 public class ApiBill extends BaseApi {
 
     public ApiBill(GatePayConfig gatePayConfig) {
         super(gatePayConfig);
     }
 
+    /**
+     * 查询账单
+     * @param request
+     * @return
+     */
     public QueryOrdersResp queryOrders(QueryOrdersReq request) {
         return super.process(request, QueryOrdersResp.class);
     }
